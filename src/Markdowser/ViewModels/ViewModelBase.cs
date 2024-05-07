6beaa9c9e8140378026613efcdc3545fdfa8fa1d
Stone_Red <56473591+Stone-Red-Code@users.noreply.github.com>
2024-05-07 18:01:24 +0200

    Update window title

--- src/Markdowser/ViewModels/ViewModelBase.cs
+++ src/Markdowser/ViewModels/ViewModelBase.cs
@@ -9,6 +9,7 @@ using Markdowser.Utilities;
 
 using ReactiveUI;
 
+using System.Diagnostics;
 using System.Reflection;
 
 namespace Markdowser.ViewModels;
@@ -16,7 +17,23 @@ namespace Markdowser.ViewModels;
 public class ViewModelBase : ReactiveObject
 {
     private WindowNotificationManager? windowNotificationManager;
-    public string Title => $"{nameof(Markdowser)} - {Assembly.GetExecutingAssembly().GetName().Version?.ToString()}";
+
+    public string Title
+    {
+        get
+        {
+            string title = nameof(Markdowser);
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string? version = fileVersionInfo.ProductVersion;
+
+#if DEBUG
+            return $"{title} - Dev {version}";
+#else
+            return $"{title} - {version}";
+#endif
+        }
+    }
 
     public WindowIcon Icon => Settings.Current.DarkMode ? new WindowIcon(AssetLoader.Open(new("avares://Markdowser/Assets/Markdowser-Dark-Transparent.ico"))) : new WindowIcon(AssetLoader.Open(new("avares://Markdowser/Assets/Markdowser-Light-Transparent.ico")));
 
